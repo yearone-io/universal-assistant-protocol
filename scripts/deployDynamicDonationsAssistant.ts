7037db594f2a832bbb3a0bdc53be0eaa03ea8a30
democrazyfi <132473036+democrazyfi@users.noreply.github.com>
2025-02-06 16:35:02 +0400

    add dynamic donations assistant deploy script

---
+++ scripts/deployDynamicDonationsAssistant.ts
@@ -0,0 +1,44 @@
+import hre, { ethers } from "hardhat";
+import { getNetworkAccountsConfig } from "../constants/network";
+
+const network = hre.network.name;
+console.log("network: ", network);
+const { UP_ADDR_CONTROLLED_BY_EOA } = getNetworkAccountsConfig(
+  network as string,
+);
+
+async function main() {
+  const deployer = UP_ADDR_CONTROLLED_BY_EOA;
+  console.log("Deploying contracts with the account:", deployer);
+  const AssistantContract = await ethers.getContractFactory(
+    "DynamicDonationAssistant",
+    {},
+  );
+  const assistantContract = await AssistantContract.deploy();
+  // wait until the contract is mined
+  await assistantContract.waitForDeployment();
+
+  // print contract address
+  const address = await assistantContract.getAddress();
+  console.log("✅ DynamicDonationAssistant deployed to:", address);
+
+  try {
+    await hre.run("verify:verify", {
+      address: assistantContract.target,
+      network,
+      constructorArguments: [],
+      contract:
+        "contracts/executive-assistants/DynamicDonationAssistant.sol:DynamicDonationAssistant",
+    });
+    console.log("Contract verified");
+  } catch (error) {
+    console.error("Contract verification failed:", error);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
